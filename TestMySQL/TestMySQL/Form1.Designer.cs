535e467e86455f427eb696f12929e0fed1b584fe
Paul Killeen <paul.killeen@gmail.com>
2021-05-11 10:29:44 +1000

    Adding data migration program

---
+++ TestMySQL/TestMySQL/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TestMySQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGet = new System.Windows.Forms.Button();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.dgvOracleUsers = new System.Windows.Forms.DataGridView();
+            this.btnMigrate = new System.Windows.Forms.Button();
+            this.btnPicks = new System.Windows.Forms.Button();
+            this.btnMySqlPicks = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOracleUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(12, 12);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(154, 35);
+            this.btnGet.TabIndex = 0;
+            this.btnGet.Text = "Get me Users from MySql";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.AllowUserToDeleteRows = false;
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Location = new System.Drawing.Point(12, 91);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.ReadOnly = true;
+            this.dgvUsers.Size = new System.Drawing.Size(521, 155);
+            this.dgvUsers.TabIndex = 1;
+            // 
+            // dgvOracleUsers
+            // 
+            this.dgvOracleUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOracleUsers.Location = new System.Drawing.Point(12, 368);
+            this.dgvOracleUsers.Name = "dgvOracleUsers";
+            this.dgvOracleUsers.Size = new System.Drawing.Size(521, 170);
+            this.dgvOracleUsers.TabIndex = 2;
+            // 
+            // btnMigrate
+            // 
+            this.btnMigrate.Location = new System.Drawing.Point(12, 301);
+            this.btnMigrate.Name = "btnMigrate";
+            this.btnMigrate.Size = new System.Drawing.Size(160, 46);
+            this.btnMigrate.TabIndex = 3;
+            this.btnMigrate.Text = "Migrate User Data";
+            this.btnMigrate.UseVisualStyleBackColor = true;
+            this.btnMigrate.Click += new System.EventHandler(this.btnMigrate_Click);
+            // 
+            // btnPicks
+            // 
+            this.btnPicks.Location = new System.Drawing.Point(217, 301);
+            this.btnPicks.Name = "btnPicks";
+            this.btnPicks.Size = new System.Drawing.Size(160, 46);
+            this.btnPicks.TabIndex = 4;
+            this.btnPicks.Text = "Migrate Picks Data";
+            this.btnPicks.UseVisualStyleBackColor = true;
+            this.btnPicks.Click += new System.EventHandler(this.btnPicks_Click);
+            // 
+            // btnMySqlPicks
+            // 
+            this.btnMySqlPicks.Location = new System.Drawing.Point(217, 12);
+            this.btnMySqlPicks.Name = "btnMySqlPicks";
+            this.btnMySqlPicks.Size = new System.Drawing.Size(154, 35);
+            this.btnMySqlPicks.TabIndex = 5;
+            this.btnMySqlPicks.Text = "Get me Picks from MySql";
+            this.btnMySqlPicks.UseVisualStyleBackColor = true;
+            this.btnMySqlPicks.Click += new System.EventHandler(this.btnMySqlPicks_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 580);
+            this.Controls.Add(this.btnMySqlPicks);
+            this.Controls.Add(this.btnPicks);
+            this.Controls.Add(this.btnMigrate);
+            this.Controls.Add(this.dgvOracleUsers);
+            this.Controls.Add(this.dgvUsers);
+            this.Controls.Add(this.btnGet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOracleUsers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.DataGridView dgvOracleUsers;
+        private System.Windows.Forms.Button btnMigrate;
+        private System.Windows.Forms.Button btnPicks;
+        private System.Windows.Forms.Button btnMySqlPicks;
+    }
+}
+
---
+++ TestMySQL/TestMySQL/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace TestMySQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string XAOConnStr = ConfigurationManager.ConnectionStrings["XAOGameDBstring"].ToString();
+        string myConnStr = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string myConnStr = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+                string strUsers = "select windows_logon, display_name, password, start_date, sound_file_name from USERS order by user_id";
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+                BindingSource bindingSauce = new BindingSource();
+
+                dataAdapter = new MySqlDataAdapter(strUsers, myConnStr);
+                DataTable table = new DataTable();
+                dataAdapter.Fill(table);
+                bindingSauce.DataSource = table;
+                dgvUsers.DataSource = bindingSauce;
+                var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+                foreach (DataRow dr in table.Rows)
+                {
+                    Console.WriteLine("pwd " + dr[2].ToString() + " = " + DecryptString(key, dr[2].ToString()) );
+                }
+                    /*
+                    using (var mySqlConnection = new MySqlConnection(myConnStr))
+                    {
+
+                        mySqlConnection.Open();
+                        MySqlCommand mySqlCmd = new MySqlCommand(strUsers, mySqlConnection);
+                        // mySqlCmd.ExecuteNonQuery();
+
+                        using (var dbread = mySqlCmd.ExecuteReader())
+                        {
+                            if (dbread.Read())
+                            {
+
+                            }
+                        }
+
+                    }
+                    */
+                }
+            catch (Exception exp)
+            {
+                Console.WriteLine("exp = " + exp.ToString());
+                MessageBox.Show("exp = " + exp.ToString(), "btnGet_Click");
+                throw;
+            }
+        }
+
+        private void InsertMySql(string strSQL)
+        {
+            // string myConnStr = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+            MySqlConnection con = new MySqlConnection(myConnStr);
+            MySqlCommand myCmd = new MySqlCommand(strSQL, con);
+            MySqlDataAdapter sda = new MySqlDataAdapter();
+            myCmd.CommandType = CommandType.Text;
+            try
+            {
+                con.Open();
+                int insertCnt = myCmd.ExecuteNonQuery();
+                Console.WriteLine("insert count = " + insertCnt.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+                sda.Dispose();
+                con.Dispose();
+            }
+
+        }
+        private DataTable GetOracleData(OracleCommand cmd, string constr)
+        {
+            DataTable dt = new DataTable();
+
+            OracleConnection con = new OracleConnection(constr);
+            OracleDataAdapter sda = new OracleDataAdapter();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            try
+            {
+                con.Open();
+                sda.SelectCommand = cmd;
+                sda.Fill(dt);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+                sda.Dispose();
+                con.Dispose();
+            }
+        }
+
+        private void btnMigrate_Click(object sender, EventArgs e)
+        {
+            DataTable dtUser = new DataTable();
+            string strUsers = "select user_id, windows_logon, display_name, password, last_logon_date, sound_file_name, picture_file_name, start_date, end_date, first_name, admin_flag, avatar_file_name from USERS order by windows_logon";
+            OracleCommand cmd = new OracleCommand(strUsers);
+            dtUser = GetOracleData(cmd, XAOConnStr);
+
+            BindingSource bindingSauce = new BindingSource();
+            DataTable table = new DataTable();
+            bindingSauce.DataSource = dtUser;
+            dgvOracleUsers.DataSource = bindingSauce;
+
+            StringBuilder sbSQL = new StringBuilder();
+            MySqlCommand myCmd = new MySqlCommand();
+            string strLastDate = string.Empty;
+            string strStartDate = string.Empty;
+            string strEndDate = string.Empty;
+            string strPwd = string.Empty;
+            var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+            Int32 iCnt = 0;
+            foreach (DataRow row in dtUser.Rows)
+            {
+                if (!CheckUserExistsMySql(Convert.ToInt32(row[0]), row[1].ToString()))
+                {
+                    strLastDate = format_to_str(row[4].ToString());
+                    strStartDate = format_to_str(row[7].ToString());
+                    strEndDate = format_to_str(row[8].ToString());
+                    strPwd = EncryptString(key,  row[3].ToString());
+                    sbSQL = new StringBuilder();
+                    sbSQL.Append("Insert into XAO_INDEX_GAME.USERS (USER_ID,WINDOWS_LOGON,DISPLAY_NAME,PASSWORD,LAST_LOGON_DATE, ");
+                    sbSQL.Append("  SOUND_FILE_NAME,PICTURE_FILE_NAME,START_DATE,END_DATE,FIRST_NAME,ADMIN_FLAG,AVATAR_FILE_NAME) values (");
+                    sbSQL.Append(row[0] + ",'" + row[1] + "','");
+                    sbSQL.Append(row[2].ToString().Replace("'", "''") + "','" + strPwd + "', " + strLastDate + ",'");
+                    sbSQL.Append(row[5].ToString().Replace("'", "''") + "','"); // the replace is to handle embedded single quotes
+                    sbSQL.Append(row[6].ToString().Replace("'", "''") + "', ");
+                    sbSQL.Append(strStartDate + " , " + strEndDate + ",'" + row[9] + "','" + row[10] + "','" + row[11] + "')");
+
+                    InsertMySql(sbSQL.ToString());
+                }
+                iCnt++;
+                Console.Write("iCnt = " + iCnt.ToString() + " column1 = " + row[0].ToString());
+
+                Console.Write(" column2 = " + row[1].ToString());
+                Console.Write(" column3 = " + row[2].ToString());
+                Console.WriteLine(" column4 = " + row[3].ToString());
+            }
+
+        }
+
+        private string format_to_str(string inStr)
+        {
+            string outStr = "str_to_date('" + inStr + "','%d/%m/%Y %H:%i:%s')";
+            return outStr;
+        }
+
+        private bool CheckUserExistsMySql(Int32 user_id, string strUsername)
+        {
+            string myConnStr = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+            string strUsers = "select user_id from USERS where user_id = " + user_id + " and windows_logon = '" + strUsername + "'";
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+            BindingSource bindingSauce = new BindingSource();
+
+            dataAdapter = new MySqlDataAdapter(strUsers, myConnStr);
+            DataTable table = new DataTable();
+            dataAdapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+                return true;
+            else
+                return false;
+
+        }
+
+        private void btnPicks_Click(object sender, EventArgs e)
+        {
+            DataTable dtUser = new DataTable();
+            string strUsers = "select pick_id, user_id, windows_logon, display_name, pick, winner_flag, jackpot_flag, jackpot_amount, pick_date from PICKS order by pick_id";
+            OracleCommand cmd = new OracleCommand(strUsers);
+            dtUser = GetOracleData(cmd, XAOConnStr);
+
+            BindingSource bindingSauce = new BindingSource();
+            DataTable table = new DataTable();
+            bindingSauce.DataSource = dtUser;
+            dgvOracleUsers.DataSource = bindingSauce;
+
+            StringBuilder sbSQL = new StringBuilder();
+            MySqlCommand myCmd = new MySqlCommand();
+            string strPickDate = string.Empty;
+
+            Int32 iCnt = 0;
+            foreach (DataRow row in dtUser.Rows)
+            {
+                if (!CheckPickExistsMySql(Convert.ToInt32(row[1]), Convert.ToInt32( row[0])))
+                {
+                    strPickDate = format_to_str(row[8].ToString());
+                    sbSQL = new StringBuilder();
+                    sbSQL.Append("Insert into XAO_INDEX_GAME.PICKS (PICK_ID,USER_ID,WINDOWS_LOGON,DISPLAY_NAME,PICK,WINNER_FLAG, ");
+                    sbSQL.Append("  JACKPOT_FLAG,JACKPOT_AMOUNT,PICK_DATE) values (");
+                    sbSQL.Append(row[0] + "," + row[1] + ",'");
+                    sbSQL.Append(row[2].ToString().Replace("'", "''") + "','" + row[3].ToString().Replace("'", "''") + "', " + row[4] + ",'");
+                    sbSQL.Append(row[5].ToString() + "','"); // winner flag
+                    sbSQL.Append(row[6] + "', "); // jackpot flag
+                    sbSQL.Append(row[7] + ", ");
+                    sbSQL.Append(strPickDate + ")");
+
+                    InsertMySql(sbSQL.ToString());
+                }
+                iCnt++;
+                if(iCnt % 6 > 4)
+                    Application.DoEvents();
+                Console.Write("iCnt = " + iCnt.ToString() + " column1 = " + row[0].ToString());
+
+                Console.Write(" column2 = " + row[2].ToString());
+                Console.Write(" column3 = " + row[3].ToString());
+                Console.WriteLine(" column4 = " + row[8].ToString());  
+            }
+        }
+
+        private bool CheckPickExistsMySql(Int32 user_id, Int32 pick_id)
+        {
+            string myConnStr = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+            string strUsers = "select user_id from PICKS where user_id = " + user_id + " and pick_id = " + pick_id;
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+            BindingSource bindingSauce = new BindingSource();
+
+            dataAdapter = new MySqlDataAdapter(strUsers, myConnStr);
+            DataTable table = new DataTable();
+            dataAdapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+                return true;
+            else
+                return false;
+
+        }
+
+
+        public static string EncryptString(string key, string plainText)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public static string DecryptString(string key, string cipherText)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnMySqlPicks_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
