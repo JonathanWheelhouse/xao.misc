4c3643fea053cf10d4016e6baad983e7618b3e83
paul.killeen@gmail.com <ghB@ckd00r>
2021-05-20 15:23:20 +1000

    Adding Index Simulator

---
+++ TestMySQL/IndexSimulator/frmIndexSimulator.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace IndexSimulator
+{
+    partial class frmIndexSimulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpCloseMkt = new System.Windows.Forms.DateTimePicker();
+            this.dtpMktOpen = new System.Windows.Forms.DateTimePicker();
+            this.btnCloseMkt = new System.Windows.Forms.Button();
+            this.btnMktOpen = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.txtIndexMove = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLoopNum = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIndexValue = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLoopCnt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIndexCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtpCloseMkt
+            // 
+            this.dtpCloseMkt.CustomFormat = "dd-MMM-yyyy HH:mm:ss ";
+            this.dtpCloseMkt.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpCloseMkt.Location = new System.Drawing.Point(298, 138);
+            this.dtpCloseMkt.Name = "dtpCloseMkt";
+            this.dtpCloseMkt.Size = new System.Drawing.Size(151, 20);
+            this.dtpCloseMkt.TabIndex = 41;
+            // 
+            // dtpMktOpen
+            // 
+            this.dtpMktOpen.CustomFormat = "dd-MMM-yyyy HH:mm:ss ";
+            this.dtpMktOpen.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpMktOpen.Location = new System.Drawing.Point(298, 76);
+            this.dtpMktOpen.Name = "dtpMktOpen";
+            this.dtpMktOpen.Size = new System.Drawing.Size(151, 20);
+            this.dtpMktOpen.TabIndex = 40;
+            // 
+            // btnCloseMkt
+            // 
+            this.btnCloseMkt.Location = new System.Drawing.Point(167, 131);
+            this.btnCloseMkt.Name = "btnCloseMkt";
+            this.btnCloseMkt.Size = new System.Drawing.Size(107, 34);
+            this.btnCloseMkt.TabIndex = 39;
+            this.btnCloseMkt.Text = "Close Market";
+            this.btnCloseMkt.UseVisualStyleBackColor = true;
+            this.btnCloseMkt.Click += new System.EventHandler(this.btnCloseMkt_Click);
+            // 
+            // btnMktOpen
+            // 
+            this.btnMktOpen.Location = new System.Drawing.Point(167, 69);
+            this.btnMktOpen.Name = "btnMktOpen";
+            this.btnMktOpen.Size = new System.Drawing.Size(107, 34);
+            this.btnMktOpen.TabIndex = 38;
+            this.btnMktOpen.Text = "Open Market";
+            this.btnMktOpen.UseVisualStyleBackColor = true;
+            this.btnMktOpen.Click += new System.EventHandler(this.btnMktOpen_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(421, 370);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(107, 34);
+            this.btnResume.TabIndex = 37;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // txtIndexMove
+            // 
+            this.txtIndexMove.Location = new System.Drawing.Point(463, 289);
+            this.txtIndexMove.Name = "txtIndexMove";
+            this.txtIndexMove.ReadOnly = true;
+            this.txtIndexMove.Size = new System.Drawing.Size(83, 20);
+            this.txtIndexMove.TabIndex = 36;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(329, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 20);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Index Movement";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtLoopNum
+            // 
+            this.txtLoopNum.Location = new System.Drawing.Point(463, 326);
+            this.txtLoopNum.Name = "txtLoopNum";
+            this.txtLoopNum.ReadOnly = true;
+            this.txtLoopNum.Size = new System.Drawing.Size(83, 20);
+            this.txtLoopNum.TabIndex = 34;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(329, 326);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 20);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Loop Count";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtIndexValue
+            // 
+            this.txtIndexValue.Location = new System.Drawing.Point(463, 251);
+            this.txtIndexValue.Name = "txtIndexValue";
+            this.txtIndexValue.ReadOnly = true;
+            this.txtIndexValue.Size = new System.Drawing.Size(83, 20);
+            this.txtIndexValue.TabIndex = 32;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(329, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 20);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Index Value";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtLoopCnt
+            // 
+            this.txtLoopCnt.Location = new System.Drawing.Point(222, 326);
+            this.txtLoopCnt.Name = "txtLoopCnt";
+            this.txtLoopCnt.Size = new System.Drawing.Size(83, 20);
+            this.txtLoopCnt.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(88, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 20);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Number of Loops";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(490, 453);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(62, 34);
+            this.btnExit.TabIndex = 29;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(291, 370);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(107, 34);
+            this.btnStop.TabIndex = 28;
+            this.btnStop.Text = "Pause";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(150, 370);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(107, 34);
+            this.btnStart.TabIndex = 27;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Location = new System.Drawing.Point(222, 289);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(83, 20);
+            this.txtInterval.TabIndex = 24;
+            this.txtInterval.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(88, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 20);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Time Interval";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtIndexCode
+            // 
+            this.txtIndexCode.Location = new System.Drawing.Point(222, 252);
+            this.txtIndexCode.Name = "txtIndexCode";
+            this.txtIndexCode.Size = new System.Drawing.Size(83, 20);
+            this.txtIndexCode.TabIndex = 23;
+            this.txtIndexCode.Text = "XAO";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(88, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 20);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Index Code";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // frmIndexSimulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 556);
+            this.Controls.Add(this.dtpCloseMkt);
+            this.Controls.Add(this.dtpMktOpen);
+            this.Controls.Add(this.btnCloseMkt);
+            this.Controls.Add(this.btnMktOpen);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.txtIndexMove);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtLoopNum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtIndexValue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLoopCnt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtInterval);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIndexCode);
+            this.Controls.Add(this.label1);
+            this.Name = "frmIndexSimulator";
+            this.Text = "Index Stimulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpCloseMkt;
+        private System.Windows.Forms.DateTimePicker dtpMktOpen;
+        private System.Windows.Forms.Button btnCloseMkt;
+        private System.Windows.Forms.Button btnMktOpen;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.TextBox txtIndexMove;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtLoopNum;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtIndexValue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLoopCnt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIndexCode;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TestMySQL/IndexSimulator/frmIndexSimulator.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Threading;
+using MySql.Data.MySqlClient;
+
+namespace IndexSimulator
+{
+    public partial class frmIndexSimulator : Form
+    {
+
+        public frmIndexSimulator()
+        {
+            InitializeComponent();
+        }
+
+        string XAOConnStr = ConfigurationManager.ConnectionStrings["XAOGameDBstring"].ToString();
+        string myConnStr = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+        Thread thIndex;
+        string strIndexCode = string.Empty;
+        IndexCalc CalcIndex;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int intLoopCnt = 0;
+            Int32 intDelayMillisecs = 0;
+
+            if (txtIndexCode.Text.Length < 3)
+            {
+                MessageBox.Show("Please enter 3 character Index code.");
+                txtIndexCode.Focus();
+                return;
+            }
+
+            txtIndexCode.Text.ToUpper();
+            strIndexCode = txtIndexCode.Text;
+
+            if (txtInterval.Text.Length < 1)
+            {
+                MessageBox.Show("Please enter a time interval in seconds");
+                txtInterval.Focus();
+                return;
+            }
+
+            if (txtInterval.Text.Length > 0)
+            {
+                if (!Int32.TryParse(txtInterval.Text, out intDelayMillisecs))
+                {
+                    MessageBox.Show("Please enter a numeric value");
+                    txtInterval.Focus();
+                    return;
+                }
+                intDelayMillisecs = intDelayMillisecs * 1000;
+            }
+
+            if (txtLoopCnt.Text.Length > 0)
+            {
+                if (!int.TryParse(txtLoopCnt.Text, out intLoopCnt))
+                {
+                    MessageBox.Show("Please enter a numeric value");
+                    txtLoopCnt.Focus();
+                    return;
+                }
+            }
+
+            CalcParamsEntity CalcParams = new CalcParamsEntity();
+            CalcParams.intDelayMilliseconds = intDelayMillisecs;
+            CalcParams.intLoopCnt = intLoopCnt;
+            CalcParams.strIndexCode = strIndexCode;
+
+            CalcIndex = new IndexCalc(CalcParams);
+            // Create the thread object, passing in the CalcIndex.IndexGenLoop method
+            // via a ThreadStart delegate. This does not start the thread.
+            thIndex = new Thread(new ThreadStart(CalcIndex.IndexGenLoop));
+            thIndex.IsBackground = true; // this sets the thread so that it dies when the application is closed.
+
+            // CalcIndex.inForm(this);
+
+            // Start the thread
+            thIndex.Start();
+        }
+
+        private void btnMktOpen_Click(object sender, EventArgs e)
+        {
+            StringBuilder sbQuery = null;
+
+            sbQuery = new StringBuilder();
+
+            // sql that will update database to set market to open.
+            sbQuery.Append("Update XAO_INDEX_GAME.SYSTEM_STATUS set market_open_Flag = 'T',   ");
+            sbQuery.Append(" market_open_timestamp = str_to_date('" + dtpMktOpen.Text.Trim() + "'");
+            sbQuery.Append(",'%d-%b-%Y %H:%i:%s') where node_name = 'DOIP203' ");
+
+            Console.WriteLine("sbQuery = " + sbQuery.ToString());
+
+
+            try
+            {
+                // DataSet that will hold the returned results		
+                int recCnt = ExecuteNonQryMySql(sbQuery.ToString());
+
+                if (recCnt > 0)
+                {
+                    MessageBox.Show("Market set to Open.");
+                }
+                else // 
+                {
+                    MessageBox.Show("Something went wrong.");
+                }
+
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Error in btnMktOpen_Click : " + exp.Message);
+            }
+
+        }
+
+        private int ExecuteNonQryMySql(string strSQL)
+        {
+            // string myConnStr = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+            MySqlConnection con = new MySqlConnection(myConnStr);
+            MySqlCommand myCmd = new MySqlCommand(strSQL, con);
+            MySqlDataAdapter sda = new MySqlDataAdapter();
+            myCmd.CommandType = CommandType.Text;
+            try
+            {
+                con.Open();
+                int insertCnt = myCmd.ExecuteNonQuery();
+                Console.WriteLine("insert count = " + insertCnt.ToString());
+                return insertCnt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+                sda.Dispose();
+                con.Dispose();
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (thIndex != null && thIndex.IsAlive)
+                thIndex.Abort();
+            this.Close();
+        }
+
+
+        public class IndexCalc
+        {
+            CalcParamsEntity lclParams;
+            bool calcValues;
+            string strClosePrice = string.Empty;
+            string strLastPrice = string.Empty;
+            frmIndexSimulator myForm;
+
+            public IndexCalc(CalcParamsEntity par)
+            {
+                this.calcValues = true;
+                this.lclParams = par;
+            }
+
+            public bool SetCalc(bool calc)
+            {
+                bool retVal = this.calcValues;
+
+                this.calcValues = calc;
+
+                return retVal;
+            }
+
+            //public void inForm(frmIndexSimulator formObj)
+            //{
+            //    myForm = formObj;
+            //}
+
+            public void IndexGenLoop()
+            {
+                int intLoopCnt = lclParams.intLoopCnt;
+                int intDelayMillisecs = lclParams.intDelayMilliseconds;
+                string strIndexCode = lclParams.strIndexCode;
+
+                if (intLoopCnt > 0)
+                {
+                    for (int i = 0; i < intLoopCnt; i++)
+                    {
+                        if (this.calcValues)
+                        {
+                            GenerateIndexValue(strIndexCode);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No calc performed");
+                        }
+                        //txtLoopNum.Text = Convert.ToString(i + 1);
+                        System.Threading.Thread.Sleep(intDelayMillisecs);
+                    }
+                }
+            }
+
+
+            private void GenerateIndexValue(string strIndexCode)
+            {
+                // ResponseMessage RespObj = new ResponseMessage();
+                double dblRandNum = 0;
+                double dblClosePrice = 0;
+                // string strLastPrice = string.Empty;
+                // string strClosePrice = string.Empty;
+
+                Random RandNum = new Random();
+                double dblLow = 600000;
+                dblRandNum = dblLow + (RandNum.NextDouble() * 500);
+                strLastPrice = dblRandNum.ToString("0.0");
+                //txtIndexValue.Text = strLastPrice;
+                Random rndClose = new Random();
+                dblLow = -5000;
+                dblClosePrice = dblLow + (rndClose.NextDouble() * 10000);
+                strClosePrice = dblClosePrice.ToString("0.0");
+                // txtIndexMove.Text = strClosePrice;
+                // this.Refresh();
+                string strReturn = string.Empty;
+
+                try
+                {
+                    // myForm.txtIndexValue.Text = strLastPrice;
+                    // myForm.txtIndexMove.Text = strClosePrice;
+                    // myForm.Refresh();
+
+                    strReturn = SecurityMasterStore.FnUpsertSecurityMaster(strIndexCode, strClosePrice, strLastPrice);
+
+                    /*                    if (!RespObj.blnSuccess)
+                                        {
+
+                                            MessageBox.Show("Error saving Pick. Problem was : " + RespObj.OperationMessage);
+                                        }
+                                        else
+                                        {
+                                            //MessageBox.Show("Index successfully entered!", CommonStuff.cntAppName);
+                                        }  */
+
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("Error in FrmIndexSimulator.GenerateIndexValue : " + exp.Message);
+                    //this.Close();
+                }
+            }
+        }
+
+        private void btnCloseMkt_Click(object sender, EventArgs e)
+        {
+            StringBuilder sbQuery = null;
+
+            sbQuery = new StringBuilder();
+
+            sbQuery.Append("Update XAO_INDEX_GAME.SYSTEM_STATUS set market_open_Flag = 'F',   ");
+            sbQuery.Append(" market_close_timestamp = str_to_date('" +  dtpCloseMkt.Text.Trim() + "'");
+            sbQuery.Append(",'%d-%b-%Y %H:%i:%s') where node_name = 'DOIP203' ");
+
+            Console.WriteLine("sbQuery = " + sbQuery.ToString());
+
+
+            try
+            {
+                // DataSet that will hold the returned results		
+                int recCnt = ExecuteNonQryMySql(sbQuery.ToString());
+
+
+                if (recCnt > 0)
+                {
+                    MessageBox.Show("Market set to Close.");
+                }
+                else // 
+                {
+                    MessageBox.Show("Something went wrong.");
+                }
+
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Error in btnCloseMkt_Click : " + exp.Message);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            CalcIndex.SetCalc(false);
+            MessageBox.Show("Index Stimulator has been paused.");
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            CalcIndex.SetCalc(true);
+            MessageBox.Show("Index Stimulator has been resumed.");
+        }
+    }
+}
